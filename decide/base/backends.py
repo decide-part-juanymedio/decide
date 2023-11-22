e487afe63e34071f17d29a1598abefcf2e795ec8
regescmar1 <regescmar1@alum.us.es>
2023-11-22 14:04:03 +0100

    feat: añadido funcionalidad login por email
    Se ha añadido un método que hace que el usuario pueda iniciar sesion con su email. También se han incluido algunos test unitarios.

--- decide/base/backends.py
+++ decide/base/backends.py
@@ -1,7 +1,11 @@
 from django.contrib.auth.backends import ModelBackend
-
+from django.contrib.auth import get_user_model
+from django.db import models
+from django.shortcuts import get_object_or_404
 from base import mods
+from django.contrib.auth.models import User
 
+UserModel = get_user_model()
 
 class AuthBackend(ModelBackend):
     '''
@@ -13,10 +17,44 @@ class AuthBackend(ModelBackend):
     for future admin queries.
     '''
 
-    def authenticate(self, request, username=None, password=None, **kwargs):
-        u = super().authenticate(request, username=username,
-                                 password=password, **kwargs)
+    def authenticateUsernameOrEmail(self, request, username=None, password=None, **kwargs):
+        """
+        Esta función segun los datos que se introduzcan en el formulario de login 
+        accederá al usuario bien por su username o bien por su email.
 
+        :param username: puede ser el username o el email del usuario
+        :type username: str
+        :param password: la password del usuario
+        :type password: str
+        :return: el user que corresponda al username o el email dado
+        :rtype: User
+        """
+
+        if username is None:
+            if '@' in username:
+                username = kwargs.get(UserModel.EMAIL_FIELD)
+            else:
+                username = kwargs.get(UserModel.USERNAME_FIELD)
+
+        if username is None or password is None:
+            return
+        try:
+            if '@' in username:
+                user = get_object_or_404(User, email=username)
+            else:
+                user = UserModel._default_manager.get_by_natural_key(username)
+        except UserModel.DoesNotExist:
+            
+            UserModel().set_password(password)
+        else:
+            if user.check_password(password)and self.user_can_authenticate(user):
+                return user
+            
+
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        #u = super().authenticate(request, username=username,
+          #                       password=password, **kwargs)
+        u=self.authenticateUsernameOrEmail(request, username=username, password=password, **kwargs)
         # only doing this for the admin web interface
         if u and request.content_type == 'application/x-www-form-urlencoded':
             data = {
--- decide/base/tests.py
+++ decide/base/tests.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.models import User
 from rest_framework.test import APIClient
 from rest_framework.test import APITestCase
-
+from django.test import TestCase
+import ipdb
 from base import mods
 
 
@@ -34,3 +35,111 @@ class BaseTestCase(APITestCase):
 
     def logout(self):
         self.client.credentials()
+
+
+class LoginWithEmail(TestCase):
+
+    def setUp(self):
+        user1 = User(username='user1',email='user1@mail.com')
+        user1.set_password('111')
+        user1.save()
+
+        user2 = User(username='user2',email='user2@mail.com')
+        user2.set_password('222')
+        user2.save()
+
+        user3 = User(username='user3',email='user3@mail.com')
+        user3.set_password('333')
+        user3.save()
+
+        user4 = User(username='user4',email='user4@mail.com')
+        user4.set_password('444')
+        user4.save()
+
+        user5 = User(username='user5',email='user5@mail.com')
+        user5.set_password('555')
+        user5.save()
+        
+        user6 = User(username='user6',email='user6@mail.com')
+        user6.set_password('666')
+        user6.save()
+
+        user7 = User(username='user7',email='user7@mail.com')
+        user7.set_password('777')
+        user7.save()
+
+        user8 = User(username='user8',email='user8@mail.com')
+        user8.set_password('888')
+        user8.save()
+
+        user9 = User(username='user9',email='user9@mail.com')
+        user9.set_password('999')
+        user9.save()
+
+        user10 = User(username='user10',email='user10@mail.com')
+        user10.set_password('101010')
+        user10.save()
+        
+    def tearDown(self):
+        return super().tearDown()
+        self.user1=None
+        self.user2=None
+        self.user3=None
+        self.user4=None
+        self.user5=None
+        self.user6=None
+        self.user7=None
+        self.user8=None
+        self.user9=None
+        self.user10=None
+    
+    def test_login_wit_email(self):
+        data = {'username': 'user1@mail.com', 'password': '111'}
+        response = self.client.post('/authentication/login/', data, format='json')
+        self.assertEqual(response.status_code, 200)
+        token = response.json()
+
+        data = {'username': 'user2@mail.com', 'password': '222'}
+        response = self.client.post('/authentication/login/', data, format='json')
+        self.assertEqual(response.status_code, 200)
+        token = response.json()
+
+        data = {'username': 'user3@mail.com', 'password': '333'}
+        response = self.client.post('/authentication/login/', data, format='json')
+        self.assertEqual(response.status_code, 200)
+        token = response.json()
+
+        data = {'username': 'user4@mail.com', 'password': '444'}
+        response = self.client.post('/authentication/login/', data, format='json')
+        self.assertEqual(response.status_code, 200)
+        token = response.json()
+
+        data = {'username': 'user5@mail.com', 'password': '555'}
+        response = self.client.post('/authentication/login/', data, format='json')
+        self.assertEqual(response.status_code, 200)
+        token = response.json()
+
+        data = {'username': 'user6@mail.com', 'password': '666'}
+        response = self.client.post('/authentication/login/', data, format='json')
+        self.assertEqual(response.status_code, 200)
+        token = response.json()
+
+        data = {'username': 'user7@mail.com', 'password': '777'}
+        response = self.client.post('/authentication/login/', data, format='json')
+        self.assertEqual(response.status_code, 200)
+        token = response.json()
+
+        data = {'username': 'user8@mail.com', 'password': '888'}
+        response = self.client.post('/authentication/login/', data, format='json')
+        self.assertEqual(response.status_code, 200)
+        token = response.json()
+
+        data = {'username': 'user9@mail.com', 'password': '999'}
+        response = self.client.post('/authentication/login/', data, format='json')
+        self.assertEqual(response.status_code, 200)
+        token = response.json()
+
+        data = {'username': 'user10@mail.com', 'password': '101010'}
+        response = self.client.post('/authentication/login/', data, format='json')
+        self.assertEqual(response.status_code, 200)
+        token = response.json()
